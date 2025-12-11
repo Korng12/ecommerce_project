1ed7f15ecf4d0308c435dcd1c1b5b5062bc8defe
Hak Sengkea <hsengkea@gmail.com>
2025-12-11 22:26:43 +0700

    Cart & Checkout Page

---
+++ frontend/src/components/cart/OrderSummary.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import { useRoute } from 'vue-router';
+defineProps({
+  subtotal: Number,
+  shipping: Number,
+  tax: Number
+});
+const path = useRoute().path;
+</script>
+
+<template>
+  <div class="p-6 bg-white shadow-lg rounded-2xl h-fit">
+    <p class="text-xl font-bold mb-5">Order Summary</p>
+
+    <div class="flex justify-between py-2">
+      <p>Subtotal</p><p>${{ subtotal }}</p>
+    </div>
+
+    <div class="flex justify-between py-2">
+      <p>Standard Shipping</p><p>${{ shipping }}</p>
+    </div>
+
+    <div class="flex justify-between py-2">
+      <p>Tax</p><p>${{ tax }}</p>
+    </div>
+
+    <hr class="my-4" />
+
+    <div class="flex justify-between font-bold text-lg">
+      <p>Total</p><p>${{ subtotal + shipping + tax }}</p>
+    </div>
+
+    <button class="w-full mt-4 bg-blue-700 text-white py-3 rounded-lg">
+      <RouterLink to="/checkout">
+        {{ path === '/checkout' ? 'Pay Now' : 'Checkout' }}
+      </RouterLink>
+    </button>
+  </div>
+</template>
---
+++ frontend/src/views/CartView.vue
@@ -0,0 +1,24 @@
+<!-- CartPage.vue -->
+<script setup>
+import { ref } from "vue";
+import CartList from "@/components/cart/CartList.vue";
+import OrderSummary from "@/components/cart/OrderSummary.vue";
+import mouse from "@/assets/image/mouse.png";
+
+const cartItems = ref([
+  { id: 1, name: "Acer Mouse", color: "Black", type: "Wireless", desc: "Desc...", img: mouse, qty: 1, price: 1200 },
+  { id: 2, name: "Acer Mouse", color: "Black", type: "Wireless", desc: "Desc...", img: mouse, qty: 1, price: 1200 },
+]);
+</script>
+
+<template>
+  <section class="flex gap-10 p-10">
+    <div class="w-2/3">
+      <CartList :items="cartItems" />
+    </div>
+
+    <div class="w-1/3">
+      <OrderSummary :subtotal="2400" :shipping="15" :tax="120" />
+    </div>
+  </section>
+</template>
