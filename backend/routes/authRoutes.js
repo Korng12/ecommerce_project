ce269c06f4b750a579df0443ab74672e57c1affc
Korng12 <chanthamengkong@gmail.com>
2026-01-07 13:25:46 -0500

    config using common js within backend project

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { register, login } from '../controllers/authController.js';
-
+// import express from 'express';
+// import { register, login } from '../controllers/authController.js';
+const {register,login} = require('../controllers/authController')
+const express = require('express');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
-export default router;
+module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
 
-dotenv.config();
-
+require('dotenv').config()
+const express=require('express')
 const app = express();
-
+const cors=require('cors')
 app.use(cors());
 app.use(express.json());               // ✅ REQUIRED
 app.use(express.urlencoded({ extended: true }));
-
+const authRoutes = require('./routes/authRoutes')
 app.use('/api', authRoutes);
-
+app.use('/hello',(req,res)=>{
+  console.log(req.url)
+  res.status(200).json("Hello")
+});
 app.listen(3000, () => {
   console.log('🚀 Server running on port 3000');
 });
